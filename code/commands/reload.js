00b07925c53639349d558717ec89daf804e42fa8
Dean Summer <deanlovesmargie@gmail.com>
2020-08-14 15:38:32 -0500

    Create reload.js

---
+++ code/commands/reload.js
@@ -0,0 +1,28 @@
+module.exports = {
+    name: 'reload',
+    description: 'Reloads a command.',
+    args: true,
+    usage: '<command>',
+    guildOnly: false,
+    cooldown: 15,
+    aliases: ['refresh'],
+    execute(message, args) {
+        const commandName = args[0].toLowerCase();
+        const command = message.client.commands.get(commandName)
+            || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+        
+        if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+
+        delete require.cache[require.resolve(`./${command.name}.js`)];
+
+        try {
+            const newCommand = require(`./${command.name}.js`);
+            message.client.commands.set(newCommand.name, newCommand);
+        } catch (error) {
+            console.log(error);
+            message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+        }
+
+        message.channel.send(`Command \`${command.name}\` was reloaded!`);
+    },
+};
