a7281934c9984c89da20b6774fab9c4a8428deed
Dean Summer <deanlovesmargie@gmail.com>
2020-08-16 12:05:45 -0500

    Fix reply

--- code/commands/help.js
+++ code/commands/help.js
@@ -46,7 +46,7 @@ module.exports = {
         return message.author.send(data, { split: true })
             .then(() => {
                 if (message.channel.type === 'dm') return;
-                message.reply('I\'ve sent you a DM with all my commands!');
+                message.reply('I\'ve sent you a DM with information about that command!');
             })
             .catch(error => {
                 console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
