ff71639fbc9ecc3c234fcc2b29c72d55f4c0e903
Dean Summer <deanlovesmargie@gmail.com>
2020-08-16 12:04:15 -0500

    Put command information in DM

--- code/commands/help.js
+++ code/commands/help.js
@@ -43,6 +43,14 @@ module.exports = {
         
         data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
         
-        message.channel.send(data, { split: true });
+        return message.author.send(data, { split: true })
+            .then(() => {
+                if (message.channel.type === 'dm') return;
+                message.reply('I\'ve sent you a DM with all my commands!');
+            })
+            .catch(error => {
+                console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+                message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+            });
     },
 };
