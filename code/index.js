8f2783e5d325c3f29d290c2f5455456e792215a3
Dean Summer <deanlovesmargie@gmail.com>
2020-08-16 13:19:46 -0500

    Shard the bot

---
+++ code/index.js
@@ -0,0 +1,6 @@
+const { token } = require('./config.json');
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager('./bot.js', { token: token });
+
+manager.spawn();
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
