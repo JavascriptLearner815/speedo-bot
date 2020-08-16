14b78b1c2e82d26f877f59a61982efe460972010
Dean Summer <deanlovesmargie@gmail.com>
2020-08-16 14:45:14 -0500

    Fix typo I somehow never saw

--- code/commands/avatar.js
+++ code/commands/avatar.js
@@ -8,7 +8,7 @@ module.exports = {
     aliases: ['icon', 'pfp'],
     execute(message, args) {
         if (!message.mentions.users.size) {
-            return message.channel.send(`Your avavtar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
         }
 
         const avatarList = message.mentions.users.map(user => {
